bdb05c47f279f53a43be461e7f966dce9746c95f
David S. Rivero <drivero1999@javerianacali.edu.co>
2018-09-27 23:11:14 -0500

    Add files via upload

---
+++ PaginaPhP-27-08-18/conexion_bd.php
@@ -0,0 +1,9 @@
+<?php
+	$mysqli = new mysqli('localhost','root','','mi_revista');
+	$mysqli -> set_charset("utf8");
+	$query = $mysqli -> query("SELECT * FROM ciudad");
+
+	$query2 = $mysqli -> query("SELECT * FROM revista");
+
+
+?>
---
+++ PaginaPhP-27-08-18/subject.js
@@ -0,0 +1,47 @@
+function searchTupla(){
+	let ident = document.getElementById("search").value;
+    let dataEn = 'ident='+ident; 
+
+    $.ajax({
+    	type: 'GET',
+    	url: 'backend.php',
+    	data: dataEn,
+    	success:function(resp){
+    		$('#res').html(resp)
+    	}
+    });
+}
+
+function insertUser(){
+	let ident = document.getElementById("identification").value;
+	let f_name = document.getElementById("first_name").value;
+	let l_name = document.getElementById("last_name").value;
+	let age = document.getElementById("age").value;
+	let city = document.getElementById("city").value;
+
+	let dataEn = {
+		'ident' : ident,
+		'f_name' : f_name,
+		'l_name' : l_name,
+		'age' : age,
+		'city' : city
+	};
+
+	/*$.ajax({
+	 	type: 'POST',
+    	url: 'backend.php',
+    	data: dataEn,
+    	success:function(resp){
+    		$('#res').html(resp)
+    	}
+	});*/
+}
+
+function suscribeUser(){
+	let ident = document.getElementById("identification_sus").value;
+	let rev = document.getElementById("magazine").value;
+	let dataEn = {
+		'ident' : ident,
+		'rev' : rev
+	};
+}
